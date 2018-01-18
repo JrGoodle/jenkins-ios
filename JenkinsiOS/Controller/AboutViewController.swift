a04e78e05a50eb954d46c2ea592a085fc08f9fa7
Joe DeCapo <joe@polka.cat>
2018-01-17 22:23:21 -0600

    Scrolling info screen works

--- JenkinsiOS/Controller/AboutViewController.swift
+++ JenkinsiOS/Controller/AboutViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
-    @IBOutlet var aboutTextView: UITextView!
-    @IBOutlet var creditsTextView: UITextView!
+    @IBOutlet weak var aboutTextView: UITextView!
+    @IBOutlet weak var creditsTextView: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var aboutViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var creditsViewHeight: NSLayoutConstraint!
     
     let aboutInformationManager = AboutInformationManager()
     var reviewHandler: ReviewHandler?
@@ -29,5 +33,24 @@ class AboutViewController: UIViewController {
         creditsTextView.text = aboutInformationManager.getCreditsText() ?? ""
         
         reviewHandler = ReviewHandler(presentOn: self)
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+//        aboutTextView.sizeToFit()
+        var previousSize = CGSize(width: aboutTextView.frame.size.width,
+                                  height: CGFloat(MAXFLOAT))
+        var newSize = aboutTextView.sizeThatFits(previousSize)
+        aboutViewHeight.constant = newSize.height
+//        creditsTextView.sizeToFit()
+        previousSize = CGSize(width: creditsTextView.frame.size.width,
+                             height: CGFloat(MAXFLOAT))
+        newSize = creditsTextView.sizeThatFits(previousSize)
+        creditsViewHeight.constant = newSize.height
+
+        scrollView.contentSize = stackView.frame.size
+//        view.setNeedsLayout()
+//        view.layoutIfNeeded()
     }
 }
